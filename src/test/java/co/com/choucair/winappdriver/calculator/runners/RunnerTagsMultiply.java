ed7479d8d01faea90f61236d1858e30a8ea55357
Jose Manuel Echeverri Palacio <jose.echeverri@upb.edu.co>
2020-04-01 14:35:05 -0500

    Refactor duplicated steps

---
+++ src/test/java/co/com/choucair/winappdriver/calculator/runners/RunnerTagsMultiply.java
@@ -0,0 +1,16 @@
+package co.com.choucair.winappdriver.calculator.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/add.feature",
+        glue = {"co.com.choucair.winappdriver.calculator.stepdefinitions","co.com.choucair.winappdriver.calculator.utils"},
+        snippets = SnippetType.CAMELCASE
+)
+public class RunnerTagsAdd {
+}
